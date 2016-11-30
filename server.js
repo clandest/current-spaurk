ebae15f66dca75685849912c23c86c6e9ff098b8
Devyn <devyn.challman@gmail.com>
2016-11-30 13:44:30 -0800

    changed port variable

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ nunjucks.configure('views', {
 });
 
 //listen for a connection
-app.listen(3000, function(){
-		console.log('listening on port 3000.');
+app.listen(port, function(){
+		console.log('listening on port ' + port + '.');
 });
